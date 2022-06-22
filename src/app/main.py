882577c4e2d06896b3d97ee29ded3dd286adfb84
Hamid <hamid@MacBook-Pro.hub>
2022-06-23 09:22:48 +1000

    added async handler

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ from app.config import get_settings, Settings
 app=FastAPI()
 
 @app.get("/ping")
-def pong(settings: Settings = Depends(get_settings)):
+async def pong(settings: Settings = Depends(get_settings)):
     return {
         "ping":"pong",
         "envirnemnt":settings.environemnt,
