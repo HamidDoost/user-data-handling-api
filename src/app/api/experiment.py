188a612ec2da526638d9484332a476e797f7ca52
Hamid <hamid@MacBook-Pro.hub>
2022-07-02 14:50:13 +1000

    added experiment endpoints

---
+++ src/app/api/experiment.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import get_async_session, init_db
+from app.models.experiment_model import Experiment
+
+router = APIRouter(
+    prefix="/experiments",
+    tags=["Experiment"],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.get("/experiments", response_model=list[Experiment])
+async def get_experiments(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Experiment))
+    experiments = result.scalars().all()
+    return [Experiment(experiment_name=experiment.experiment_name, experiment_description=experiment.experiment_description, id=experiment.id) for experiment in experiments]
+
+@router.post("/experiments")
+async def add_experiment(experiment: Experiment, session: AsyncSession = Depends(get_async_session)):
+    experiment = Experiment(experiment_name=experiment.experiment_name, experiment_description=experiment.experiment_description, id=experiment.id)
+    session.add(experiment)
+    await session.commit()
+    await session.refresh(experiment)
+    return experiment
